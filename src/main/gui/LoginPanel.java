83c8f7898017cef47c3589b2299db869222af5db
damlakarakulah <damla.karakulah@ozu.edu.tr>
2017-12-11 18:48:35 +0300

    commit 223232323

--- src/main/gui/LoginPanel.java
+++ src/main/gui/LoginPanel.java
@@ -14,12 +14,34 @@ public class LoginPanel extends JPanel implements LoginListener{
     private JButton newUserButton;
 
     public LoginPanel(JFrame owner) {
-        JLabel label = new JLabel("Login Panel");
-        this.add(label);
-        setBackground(Color.red);
+        //JLabel label = new JLabel("Login Panel");
+        //this.add(label);
+        //setBackground(Color.red);
+        initializeViews();
     }
 
     private void initializeViews() {
+        JPanel mainPanel = new JPanel(new GridLayout(3,1));
+        usernameTextField = new JTextField();
+        passwordTextField = new JTextField();
+        loginButton = new JButton("Login");
+        newUserButton = new JButton("New User");
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        buttonPanel.add(loginButton);
+        buttonPanel.add(newUserButton);
+        this.setLayout(new GridBagLayout());
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayout(2,2));
+        JLabel userName = new JLabel("Username:");
+        JLabel password = new JLabel("Password:");
+        panel1.add(userName);
+        panel1.add(usernameTextField);
+        panel1.add(password);
+        panel1.add(passwordTextField);
+        mainPanel.add(panel1);
+        mainPanel.add(buttonPanel);
+        this.add(mainPanel);
 
     }
 
