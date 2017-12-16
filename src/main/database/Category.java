795cbb3c80ad84c986ff0c151ff45d0118d93d51
Batuhan Kayım <batuhankayim@gmail.com>
2017-12-17 00:14:47 +0300

    new api features, user and category classes are debugged

--- src/main/database/Category.java
+++ src/main/database/Category.java
@@ -1,19 +1,20 @@
 package main.database;
 
+import java.sql.SQLException;
 import java.util.*;
 
 public class Category {
     private int id;
     private String name;
-    private int timestamp;
+    private long timestamp;
 
-    public Category(int id, String name, int timestamp) {
+    public Category(int id, String name, long timestamp) {
         this.id = id;
         this.name = name;
         this.timestamp = timestamp;
     }
 
-    public static List<Category> getAll() {
+    public static List<Category> getAll() throws SQLException {
         return Api.getAllCategories();
     }
 
@@ -21,5 +22,5 @@ public class Category {
         return name;
     }
 
-    public int getTimestamp(){ return timestamp; }
+    public long getTimestamp(){ return timestamp; }
 }
