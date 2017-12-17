6720b62393fb05c3a7d6c19fd7dc701ec47ebf59
bengisuozkanem <32790683+bengisuozkanem@users.noreply.github.com>
2017-12-17 14:53:04 +0300

    ScoreController is added.

--- src/main/core/ScoreController.java
+++ src/main/core/ScoreController.java
@@ -1,12 +1,26 @@
 package main.core;
 
-import main.database.User;
+import main.gui.MainFrame;
+import main.gui.ScorePanel;
 
-import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class ScoreController {
-    public HashMap<User, Integer> collectQuiz() {
-        return null;
+public class ScoreController implements ActionListener {
+    private MainFrame frame;
+    private ScorePanel gui;
+
+    public ScoreController(MainFrame frame, ScorePanel gui) {
+        this.frame = frame;
+        this.gui = gui;
+        gui.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(gui.getBackButton())) {
+            frame.showLoginPage();
+        }
+
     }
 }
