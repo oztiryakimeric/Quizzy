0fa6e25572a2c3898055bcccce806ce2614b1553
Batuhan Kayım <batuhankayim@gmail.com>
2017-12-19 22:52:28 +0300

    Minor change

--- src/main/Game.java
+++ src/main/Game.java
@@ -3,7 +3,7 @@ package main;
 import main.gui.MainFrame;
 
 public class Game {
-    public static final String APP_NAME = "Quizzy7";
+    public static final String APP_NAME = "Quizzy - Welcome";
 
     public static void main(String[] args) {
         new MainFrame();
